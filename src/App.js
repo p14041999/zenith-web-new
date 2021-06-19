0cb6aaf87e37d777cf773e7500082e71563568f5
Pralay Mandal <p14041999@gmail.com>
2021-06-19 20:50:22 +0530

    Updated Cursor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import './App.scss';
+import React,{Component} from 'react';
 import Navbar from './components/Navabar';
 import Hero from './components/Hero';
 import Usage from './components/Usage'; 
 import About from './components/About';
 import Footer from './components/Footer';
-function App() {
-  return (
-    <>
-    <div className="max-width bg-dark" id="desktop">
-      <Navbar />
-      <Hero />
-      <Usage />
-      <About />
-    </div>
-    <div className="max-width pos-rel">
-      <Footer />
-    </div>
-    </>
-  );
+export default class App extends Component {
+  componentDidMount(){
+    let cursor = window.document.querySelector('.cursor');
+    let pointer = window.document.querySelector('.pointer');
+    window.document.addEventListener('mousemove',e=>{
+      // console.log(e.pageY,e.pageX)
+      cursor.setAttribute('style',`top:${e.pageY-25}px;left:${e.pageX-25}px`);
+      pointer.setAttribute('style',`top:${e.pageY-2.5}px;left:${e.pageX-2.5}px`);
+    })
+  }
+  render(){
+    return (
+      <>
+      <div className="max-width bg-dark" id="desktop">
+        <Navbar />
+        <Hero />
+        <Usage />
+        <About />
+      </div>
+      <div className="max-width pos-rel">
+        <Footer />
+      </div>
+      <span className="cursor"></span>
+      <span className="pointer"></span>
+      </>
+    );
+  }
 }
-
-export default App;
